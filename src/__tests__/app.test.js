5e25e854c1c665a14147799316d07e539cc12e18
Nicolas <nicolas.delfino@tretton37.com>
2017-06-05 15:36:10 +0200

    Add derp test

---
+++ src/__tests__/app.test.js
@@ -0,0 +1,21 @@
+// import "../tags/title.tag";
+
+describe('main state', () => {
+
+  // before(function(done) {
+  //   riot.compile(function() {
+  //     var html = document.createElement('test')
+  //     document.body.appendChild(html)
+  //     tag = riot.mount('*')[0]
+  //     done()
+  //   })
+  // });
+  //
+  it('should mount the main app', () => {
+    expect(0).toBeLessThan(1)
+  })
+  //
+  // it('mounts the title tag', () => {
+  //   let tag = riot.mount('title')[0]
+  // })
+})
--- src/app.tag
+++ src/app.tag
@@ -4,4 +4,6 @@ import "./tags/form.tag";
 <app>
   <titleComponent store={this.opts.store} />
   <testComponent store={this.opts.store}/>
+
+
 </app>
